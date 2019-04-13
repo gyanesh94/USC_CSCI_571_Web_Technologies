24f17bd036bf3aa81e004008455ef2f7c5e32636
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-04-13 01:08:35 -0700

    Search fragment view implementation

--- Ebay-Product-Search-Android/android_app/app/src/main/java/edu/gyaneshm/ebay_product_search/fragments/SearchFragment.java
+++ Ebay-Product-Search-Android/android_app/app/src/main/java/edu/gyaneshm/ebay_product_search/fragments/SearchFragment.java
@@ -7,16 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import edu.gyaneshm.ebay_product_search.R;
 
 public class SearchFragment extends Fragment {
+    private EditText mKeywordEditText;
+    private TextView mKeywordErrorTextView;
+    private Spinner mCategorySpinner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.search_fragment_layout, container, false);
 
+        mKeywordEditText = view.findViewById(R.id.search_keyword);
+        mKeywordErrorTextView = view.findViewById(R.id.search_keyword_error);
+
+        mCategorySpinner = view.findViewById(R.id.search_category);
+        initializeCategorySpinner();
+
         return view;
+    }
+
+    private void initializeCategorySpinner() {
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(
+                getActivity(),
+                R.array.search_categories,
+                android.R.layout.simple_spinner_dropdown_item
+        );
+        mCategorySpinner.setAdapter(categoryAdapter);
+        mCategorySpinner.setSelection(0);
     }
 }
